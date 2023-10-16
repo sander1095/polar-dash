7639ef512d3669de46a630d09bdfcf0780734fa9
Sander ten Brinke <s.tenbrinke2@gmail.com>
2023-10-16 18:56:08 +0200

    Connect to old swa-dab-test project and use its entities to test if this works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 async function getData() {
   let host = process.env.NODE_ENV === 'production' ? 'https://proud-desert-0a06b8303.3.azurestaticapps.net/' : 'http://localhost:4280/';
 
-  let res = await fetch(`${host}data-api/rest/TriviaQuestion`);
+  let res = await fetch(`${host}data-api/rest/Person`);
   let responseText = await res.text();
 
   if (!res.ok) {
@@ -13,8 +13,8 @@ async function getData() {
 }
 
 export default async function Home() {
-  const triviaQuestions = await getData();
-  let parsedJson = JSON.parse(triviaQuestions);
+  const persons = await getData();
+  let parsedJson = JSON.parse(persons);
 
   return (
     <main className="background--secondary min-h-screen flex justify-center items-center">
@@ -24,15 +24,13 @@ export default async function Home() {
         <table>
           <thead>
             <tr>
-              <th>Question</th>
-              <th>Answer</th>
+              <th>Name</th>
             </tr>
           </thead>
           <tbody>
-            {parsedJson?.value?.map((question: any) => (
-              <tr key={question.Id}>
-                <td>{question.Question}</td>
-                <td>{question.Answer}</td>
+            {parsedJson?.value?.map((person: any) => (
+              <tr key={person.Id}>
+                <td>{person.Name}</td>
               </tr>
             ))}
           </tbody>
